4bfb8f1b8b77096c1b2902de2f5201610f260c2b
ileskaa <akseli.ingervo@gmail.com>
2025-07-18 19:47:47 +0300

    Add main file

---
+++ src/main.py
@@ -0,0 +1,42 @@
+"""File used to initialize and train the network from scratch"""
+
+from mlp import MultiLayerPerceptron
+from mnist_loader import load_data
+from nn_utils import normalize_image_data
+
+if __name__ == "__main__":
+    (x_train, y_train), (x_test, y_test) = load_data()
+    x_train = normalize_image_data(x_train)
+    x_test = normalize_image_data(x_test)
+
+    # On one attempt, this yielded a 97.49% accuracy
+    # layers = [784, 256, 128, 10]
+    # model = MultiLayerPerceptron(layers)
+    # model.train(x_train, y_train, epochs=20, learning_rate=0.02, batch_size=64)
+
+    # On one attempt, this yielded a 97.44% accuracy
+    # layers = [784, 256, 128, 10]
+    # model = MultiLayerPerceptron(layers)
+    # model.train(x_train, y_train, epochs=20, learning_rate=0.025, batch_size=92)
+
+    # Was able to hit 97.63% accuracy. Test data was not even normalized
+    # layers = [784, 384, 128, 10]
+    # model = MultiLayerPerceptron(layers)
+    # model.train(x_train, y_train, epochs=20, learning_rate=0.02, batch_size=64)
+
+    # Got 97.52% accuracy on normalized data. But training was slooow
+    layers = [784, 512, 64, 10]
+    model = MultiLayerPerceptron(layers)
+    model.train(x_train, y_train, epochs=20, learning_rate=0.02, batch_size=64)
+
+    # Achieved 97.51% accuracy, but the training time was long
+    # layers = [784, 384, 128, 10]
+    # model = MultiLayerPerceptron(layers)
+    # model.train(x_train, y_train, epochs=20, learning_rate=0.015, batch_size=48)
+
+    # Hit 97.60% accuracy on mormalized test data
+    # layers = [784, 256, 256, 10]
+    # model = MultiLayerPerceptron(layers)
+    # model.train(x_train, y_train, epochs=20, learning_rate=0.02, batch_size=64)
+
+    model.measure_accuracy(x_test, y_test)
