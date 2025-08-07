fbf0c50bbf314cc818be80df6149233816de70f0
ileskaa <akseli.ingervo@gmail.com>
2025-08-07 13:55:34 +0300

    Fix random number generation issue with integration tests

--- src/model/tests/integration_test.py
+++ src/model/tests/integration_test.py
@@ -12,24 +12,24 @@ class TestModel(unittest.TestCase):
     def setUp(self) -> None:
         """Create a model to be used during testing"""
         layer_sizes = [784, 256, 128, 10]
-        self.rng = np.random.default_rng(63)
-        self.model = MultiLayerPerceptron(layer_sizes, self.rng)
+        rng = np.random.default_rng(seed=63)
+        self.model = MultiLayerPerceptron(layer_sizes, rng)
         l_bound = 0
         u_bound_exclusive = 256
         sample_size = 20
         num_pixels = 784
-        x_sample = self.rng.integers(
+        x_sample = rng.integers(
             l_bound, u_bound_exclusive, size=(sample_size, num_pixels)
         )
         # Normalize pixel values
         self.x_sample = x_sample / 255
-        self.y_sample = self.rng.integers(0, 10, sample_size)
+        self.y_sample = rng.integers(0, 10, sample_size)
 
     def test_network_overfits_small_dataset(self):
         """On a small dataset, the network should be able to achieve
         a classification accuracy of 100%"""
         self.model.train(
-            self.x_sample, self.y_sample, epochs=15, batch_size=2, learning_rate=0.02
+            self.x_sample, self.y_sample, epochs=16, batch_size=2, learning_rate=0.02
         )
         accuracy = self.model.measure_accuracy(self.x_sample, self.y_sample)
         # The model should fit the data perfectly
